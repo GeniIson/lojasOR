8dd0dad2ad95e000bd4f6cd19223a42df4f45ff0
GeniIson <122135056+GeniIson@users.noreply.github.com>
2023-06-10 12:05:27 -0300

    cadstra impresa

--- paineis/index.php
+++ paineis/index.php
@@ -7,4 +7,6 @@ include "atualiza_valores.php";
 //paine que mostara as funções
 include "painel.php";
 //modais para enserir os dados 
-include "modais/ijeta_captal.php";include "modais/compra_aparelho.php";
+include "modais/ijeta_captal.php";
+include "modais/compra_aparelho.php";
+include "modais/cadastra_empresa.php";
--- paineis/painel.php
+++ paineis/painel.php
@@ -351,60 +351,20 @@ data-bs-toggle='modal'
               Donos
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                <svg class="bi"><use xlink:href="#graph-up"/></svg>
-                Reports
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                <svg class="bi"><use xlink:href="#puzzle"/></svg>
-                Integrations
-              </a>
-            </li>
-          </ul>
-
-          <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
-            <span>Saved reports</span>
-            <a class="link-secondary" href="#" aria-label="Add a new report">
-              <svg class="bi"><use xlink:href="#plus-circle"/></svg>
-            </a>
-          </h6>
-          <ul class="nav flex-column mb-auto">
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                <svg class="bi"><use xlink:href="#file-earmark-text"/></svg>
-                Current month
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                <svg class="bi"><use xlink:href="#file-earmark-text"/></svg>
-                Last quarter
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                <svg class="bi"><use xlink:href="#file-earmark-text"/></svg>
-                Social engagement
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                <svg class="bi"><use xlink:href="#file-earmark-text"/></svg>
-                Year-end sale
-              </a>
-            </li>
-          </ul>
+         
 
           <hr class="my-3">
 
           <ul class="nav flex-column mb-auto">
             <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
+              <a class="nav-link d-flex align-items-center gap-2" href="#"
+                    
+data-bs-toggle='modal' 
+       data-bs-target='#cadastra_empresa'
+       
+       >
                 <svg class="bi"><use xlink:href="#gear-wide-connected"/></svg>
-                Configurações
+                Configurações <br>(cadastra empresa)
               </a>
             </li>
             <li class="nav-item">
@@ -436,7 +396,10 @@ data-bs-toggle='modal'
 
 
 
+      <?php  
+ include_once '../acoes/escreve-mensagem.php';
 
+?>
 
 
       <h2>Patrimonio</h2>
