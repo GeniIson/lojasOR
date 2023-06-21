6e1eb01e80dea4d7b25124e61625574ae82a218c
GeniIson <122135056+GeniIson@users.noreply.github.com>
2023-06-21 00:38:10 -0300

    aj

--- site/ler_bd/anuncios.php
+++ site/ler_bd/anuncios.php
@@ -7,18 +7,18 @@
 
 
 
-<div class="col mb-5">
-                        <div class="card h-100">
+<div class="col mb-5" style="padding: 10px;margin: 0;">
+                        <div class="card h-100" style="padding: -10px; margin: 0px; ">
                             <!-- Sale badge-->
                             <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Promoção</div>
                             
                             <!-- Product image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <img class="card-img-top" src="capas/<?=$capa_arquivo?>" alt="..." />
                             <!-- Product details-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
+                            <div class="card-body p-4" style="padding: -10px; margin: 0px; ">
+                                <div class="text-center" style="padding: -10px; margin: 0px; ">
                                     <!-- Product name-->
-                                    <h5 class="fw-bolder"><?=$descricao?></h5>
+                                    <h5 class="fw-bolder" style="padding: -10px; margin: 0px; "><?=$descricao?></h5>
                                     <!-- Product reviews-->
                                     <div class="d-flex justify-content-center small text-warning mb-2">
                                         <div class="bi-star-fill"></div>
@@ -28,17 +28,17 @@
                                         <div class="bi-star-fill"></div>
                                     </div>
                                     <!-- Product price-->
-                                  De  <span class="text-muted text-decoration-line-through"><?=$valor_novo?></span> <br>Por
+                                  De  <span class="text-muted text-decoration-line-through" style="padding: -10px; margin: 0px; "><?=$valor_novo?></span> <br>Por
                                     <?=$valor_venda?>
                                 </div>
                             </div>
                             <!-- Product actions-->
 
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent" style="padding: -10px; margin: 0px; ">
                              
 
 
-<div class="text-center"><a class="btn btn-outline-dark mt-auto" href="?produto=<?=$id_ap ?>">
+<div class="text-center"style="padding: -10px; margin: 0px; "><a class="btn btn-outline-dark mt-auto" href="?produto=<?=$id_an?>">
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-zoom-out" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
   <path d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z"/>
--- site/ler_bd/anuncios_lista.php
+++ site/ler_bd/anuncios_lista.php
@@ -2,20 +2,20 @@
 
 
   
-$sql = "SELECT * FROM aparelhos WHERE status LIKE 'venda' ORDER BY id ASC ";
+$sql = "SELECT * FROM site_anuncios WHERE data_fim LIKE '0000-00-00' ORDER BY id DESC ";
 //$sql = "SELECT * FROM  site_anuncios WHERE status LIKE 'venda' ORDER BY id ASC ";
 $resultado = mysqli_query($con, $sql);
 
 
 while($dados = mysqli_fetch_array($resultado)) { 
 // criar variaveis para cada dado do array associativo
-$id_ap   = $dados['id'];
-$tipo  = $dados['tipo'];
-$descricao   = $dados['descricao'];
-$valor_compra   = $dados['valor_compra'];
-$extra = $dados['extra'];
-$valor_venda  = $dados['valor_usado'];
-$valor_novo  = $dados['valor_novo'];
+$id_an   = $dados['id'];
+$descricao  = $dados['descricao'];
+$detalhes   = $dados['detalhes'];
+//$valor_compra   = $dados['valor_compra'];
+$capa_arquivo = $dados['capa_arquivo'];
+$valor_venda  = $dados['valo_de_venda'];
+$valor_novo  = $dados['valor_de_novo'];
 
 
 
